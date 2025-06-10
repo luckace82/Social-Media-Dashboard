a705494301504dff4f6a807e81155de91e2ed823
luckace82 <lakesshrestha82@gmail.com>
2025-06-10 23:37:07 +0545

    ADDED : urls

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'home.html') # This view renders the home.html template when the home_view function is called.
+# The request parameter is the HTTP request object that Django passes to the view function.
